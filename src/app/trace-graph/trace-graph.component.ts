a64136d35027b6c3d1673ba42f44965e04289447
Jinfeng Lin <ljfnwpusife@gmail.com>
2018-01-01 23:50:24 -0500

    switch to vis.js successfully ran example in angular, moving forward to draw

--- src/app/trace-graph/trace-graph.component.ts
+++ src/app/trace-graph/trace-graph.component.ts
@@ -1,28 +1,57 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {InputDisplayBridgeService} from '../services/input-display-bridge/input-display-bridge.service';
 import {Vertex} from '../data-structure/vertex';
 import {TraceQueryService} from '../services/trace-query/trace-query.service';
-
-declare var acl: any;
+import * as vis from 'vis';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-trace-graph',
   templateUrl: './trace-graph.component.html',
   styleUrls: ['./trace-graph.component.css']
 })
-export class TraceGraphComponent implements OnInit {
+export class TraceGraphComponent implements OnInit, AfterViewInit {
   sourceAndTarget: Vertex[][];
+  @ViewChild('traceGraph') traceGraph;
 
   constructor(private bridge: InputDisplayBridgeService, private traceProvider: TraceQueryService) {
-    this.bridge.getSourceTarget().subscribe(st => this.sourceAndTarget = st);
-    const res = traceProvider.getJsonResult(this.sourceAndTarget[0], this.sourceAndTarget[1]);
 
-    const config = {
-      dataSource: res,
-    };
-    acl.begin({'dataSource': config});
+    console.log(this.traceGraph);
+    // this.bridge.getSourceTarget().subscribe(st => this.sourceAndTarget = st);
+    // const res = traceProvider.getJsonResult(this.sourceAndTarget[0], this.sourceAndTarget[1]);
+    //
+    // const config = {
+    //   dataSource: res,
+    // };
+    // const network = new vis.Network(this.traceGraph, data, options);
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    const nodes = new vis.DataSet([
+      {id: 1, label: 'Node 1'},
+      {id: 2, label: 'Node 2'},
+      {id: 3, label: 'Node 3'},
+      {id: 4, label: 'Node 4'},
+      {id: 5, label: 'Node 5'}
+    ]);
+
+    // create an array with edges
+    const edges = new vis.DataSet([
+      {from: 1, to: 3},
+      {from: 1, to: 2},
+      {from: 2, to: 4},
+      {from: 2, to: 5},
+      {from: 3, to: 3}
+    ]);
+    console.log(this.traceGraph)
+    const data = {
+      nodes: nodes,
+      edges: edges
+    };
+    const options = {};
+    const network = new vis.Network(this.traceGraph.nativeElement, data, options);
   }
 
 
