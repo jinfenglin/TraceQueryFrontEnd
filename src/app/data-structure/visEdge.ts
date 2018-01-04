2ba1bfd40a618a05ba1cf42f3136a409b34b9e51
Jinfeng Lin <ljfnwpusife@gmail.com>
2018-01-03 21:44:56 -0500

    fixed updating issue in trace graph page

---
+++ src/app/data-structure/visEdge.ts
@@ -0,0 +1,6 @@
+export class VisEdge {
+  id: number;
+  title: string;
+  from: number;
+  to: number;
+}
--- src/app/trace-graph/trace-graph.component.ts
+++ src/app/trace-graph/trace-graph.component.ts
@@ -3,9 +3,8 @@ import {InputDisplayBridgeService} from '../services/input-display-bridge/input-
 import {Vertex} from '../data-structure/vertex';
 import {TraceQueryService} from '../services/trace-query/trace-query.service';
 import * as vis from 'vis';
-import * as $ from 'jquery';
-import {Edge} from '../data-structure/edge';
 import {VisNode} from '../data-structure/visNode';
+import {VisEdge} from '../data-structure/visEdge';
 @Component({
   selector: 'app-trace-graph',
   templateUrl: './trace-graph.component.html',
@@ -17,12 +16,10 @@ export class TraceGraphComponent implements OnInit, AfterViewInit {
   sourceAndTarget: Vertex[][];
   @ViewChild('traceGraph') traceGraph;
   selectedNode: VisNode;
-  selectedNodeEdges: Edge[];
+  selectedNodeEdges: VisEdge[];
 
   constructor(private bridge: InputDisplayBridgeService, private traceProvider: TraceQueryService) {
     this.selectedNode = {id: 2, label: 'Node 2'};
-
-    console.log(this.traceGraph);
     // this.bridge.getSourceTarget().subscribe(st => this.sourceAndTarget = st);
     // const res = traceProvider.getJsonResult(this.sourceAndTarget[0], this.sourceAndTarget[1]);
     //
@@ -36,23 +33,24 @@ export class TraceGraphComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    const raw: VisNode[] = [
+    const rawNode: VisNode[] = [
       {id: 1, label: 'Node 1'},
       {id: 2, label: 'Node 2'},
       {id: 3, label: 'Node 3'},
       {id: 4, label: 'Node 4'},
       {id: 5, label: 'Node 5'}
     ]
-    const nodes = new vis.DataSet(raw);
+    const nodes = new vis.DataSet(rawNode);
 
     // create an array with edges
-    const edges = new vis.DataSet([
+    const rawEdge: VisEdge[] = [
       {id: 1, from: 1, to: 3, title: 'score=1 \n method=default'},
       {id: 2, from: 1, to: 2, title: 'score=1 \n method=default'},
       {id: 3, from: 2, to: 4, title: 'score=1 \n method=default'},
       {id: 4, from: 2, to: 5, title: 'score=1 \n method=default'},
       {id: 5, from: 3, to: 3, title: 'score=1 \n method=default'}
-    ]);
+    ]
+    const edges = new vis.DataSet(rawEdge);
     console.log(this.traceGraph)
     const data = {
       nodes: nodes,
@@ -73,7 +71,8 @@ export class TraceGraphComponent implements OnInit, AfterViewInit {
       }
     };
     const network = new vis.Network(this.traceGraph.nativeElement, data, options);
-    network.on('selectNode', function (params) {
+    network.on('selectNode', (params) => {
+      console.log('pre node=', this.selectedNode);
       this.selectedNode = nodes.get(params.nodes[0]);
       console.log('node=', this.selectedNode);
       const edgeIds = params.edges
@@ -84,6 +83,4 @@ export class TraceGraphComponent implements OnInit, AfterViewInit {
       console.log('edges=', this.selectedNodeEdges);
     });
   }
-
-
 }
